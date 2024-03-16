86c18a55959725a50f1cbbc1885580ca88b5ad70
Nenavath Naresh <nenavathnaresh460@gmail.com>
2024-03-17 00:51:00 +0530

    handled inp radio and checkboxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crud';
+  firstname:any = 'Naresh'
+  gender:any;
+  selectedTech:any[]=[];
+  technologies:any[] = ['python','angular','react','html','css']
+  femaleImg:any = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRt03ZDzrVA93OrtiL5b818YcwLzNHlN1vug&usqp=CAU"
+  maleImg:any = 'https://storage.needpix.com/rsynced_images/symbol-of-male.jpg'
+
+  selTech(e:any){
+    console.log(e.target.value)
+    if(e.target.checked){
+      this.selectedTech.push(e.target.value)
+    }
+    else{
+      this.selectedTech = this.selectedTech.filter((tech:any)=>{
+        if(tech === e.target.value){
+          return false
+        }
+        else{
+          return true
+        }
+      })
+    }
+  }
 }
